e3b53283485b28c7c5418b6001259f436534cf96
Maftukhah Dwi Utami <38360517+maftukhahdu@users.noreply.github.com>
2019-11-23 22:29:31 +0700

    Add files via upload

---
+++ TugasAkhirDKP/TA2/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿using System;
+
+namespace TA2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.process1 = new System.Diagnostics.Process();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(492, 511);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(701, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SELESAI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(409, 251);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 30);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(716, 240);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 29);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(624, 336);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(233, 54);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(652, 500);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "YA";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(752, 500);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "TIDAK";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(336, 302);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(256, 22);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Reguler",
+            "Special",
+            "VIP",
+            "VVIP"});
+            this.comboBox1.Location = new System.Drawing.Point(408, 348);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 24);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1 Jam",
+            "2 Jam",
+            "3 Jam",
+            "4 Jam (Free Satu Jam)"});
+            this.comboBox2.Location = new System.Drawing.Point(409, 396);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(194, 24);
+            this.comboBox2.TabIndex = 18;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.comboBox3.Location = new System.Drawing.Point(324, 464);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(277, 24);
+            this.comboBox3.TabIndex = 19;
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(633, 535);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(233, 23);
+            this.progressBar1.TabIndex = 20;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(121, 382);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "LoadList";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Paket Reguler",
+            "Paket Special",
+            "Paket VIP",
+            "Paket VVIP"});
+            this.listBox1.Location = new System.Drawing.Point(38, 283);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 84);
+            this.listBox1.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(38, 422);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(245, 105);
+            this.textBox4.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(878, 586);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button6;
+        private EventHandler listBox1_SelectedIndexChanged1;
+
+        public EventHandler GetlistBox1_SelectedIndexChanged()
+        {
+            return listBox1_SelectedIndexChanged1;
+        }
+
+        private void SetlistBox1_SelectedIndexChanged(EventHandler value)
+        {
+            listBox1_SelectedIndexChanged1 = value;
+        }
+
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ TugasAkhirDKP/TA2/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int kategori;
+            int harga = 0;
+            int harga2 = 0;
+            int total = 0;
+            int paket = 0;
+            int snackdanminuman = 0;
+            kategori = comboBox1.SelectedIndex;
+            if (comboBox1.SelectedIndex == 1)
+            {
+                harga = 75000;
+                harga2 = 35000;
+                paket = comboBox2.SelectedIndex + 1;//Convert.ToInt(comboBox2.SelectedValue);
+                snackdanminuman = comboBox3.SelectedIndex + 1;
+                total = (harga * paket) + (harga2 * snackdanminuman);
+
+            }
+            else if (comboBox1.SelectedIndex == 0)
+            {
+                harga = 50000;
+                harga2 = 20000;
+                paket = comboBox2.SelectedIndex + 1;//Convert.ToInt(comboBox2.SelectedValue);
+                snackdanminuman = comboBox3.SelectedIndex + 1;
+                total = (harga * paket) + (harga2 * snackdanminuman);
+
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                harga = 100000;
+                harga2 = 50000;
+                paket = comboBox2.SelectedIndex + 1;//Convert.ToInt(comboBox2.SelectedValue);
+                snackdanminuman = comboBox3.SelectedIndex + 1;
+                total = (harga * paket) + (harga2 * snackdanminuman);
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                harga = 125000;
+                harga2 = 75000;
+                paket = comboBox2.SelectedIndex + 1;//Convert.ToInt(comboBox2.SelectedValue);
+                snackdanminuman = comboBox3.SelectedIndex + 1;
+                total = (harga * paket) + (harga2 * snackdanminuman);
+            }
+            textBox2.Text = " " + total;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int daftarmenu;
+            daftarmenu = listBox1.SelectedIndex;
+            if (listBox1.SelectedIndex == 1)
+            {
+                textBox4.Text = "1 Soft Drink (CocaCola/Fanta/Sprite)," + "\n2 Snack uk Sedang";
+            }
+            else if (listBox1.SelectedIndex == 0)
+            {
+                textBox4.Text = "1 Soft Drink (Fanta/Sprite)," + "\n 2 Snack uk Kecil";
+            }
+            else if (listBox1.SelectedIndex == 2)
+            {
+                textBox4.Text = "1 Soft Drink (Bebas Memilih)," + "\n 1 Syrup Orange," + "\n 2 Snack uk Besar";
+            }
+            else if (listBox1.SelectedIndex == 3)
+            {
+                textBox4.Text = "1 Soft Drink (Bebas Memilih)," + "\n 1 Syrup (Bebas Memilih)," + "\n 2 Snack uk Besar";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                comboBox1.SelectedIndex = -1;
+                comboBox2.SelectedIndex = -1;
+                comboBox3.SelectedIndex = -1;
+                textBox1.Text = String.Empty;
+                textBox2.Text = String.Empty;
+                textBox3.Text = String.Empty;
+                textBox4.Text = String.Empty;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "Terimakasih";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i;
+
+            progressBar1.Maximum = 200;
+            progressBar1.Minimum = 0;
+
+            for (i = 0; i <= 200; i++)
+            {
+                progressBar1.Value = i;
+            }
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            comboBox3.SelectedIndex = -1;
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+            textBox3.Text = String.Empty;
+            textBox4.Text = String.Empty;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Terimakasih sudah berkunjung di Quest Karaoke" +
+                "\nSelamat Menikmati" +
+                "\n(Created by Maft.DU)");
+        }
+    }
+}
